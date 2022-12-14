506762969cd3d4fed47b58962919b7dae6a9cc36
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-14 19:51:10 -0300

    feat: exercícios 3

--- Prática - Exercícios JOIN/01.sql
+++ Prática - Exercícios JOIN/01.sql
@@ -1,1 +1,1 @@
-SELECT users.id AS id, users.name AS name, cities.name AS city FROM users JOIN cities ON users."cityId" = cities.id WHERE cities.name = "Rio de Janeiro";
+SELECT users.id, users.name, cities.name AS city FROM users JOIN cities ON users."cityId" = cities.id WHERE cities.name = "Rio de Janeiro";
--- Prática - Exercícios JOIN/02.sql
+++ Prática - Exercícios JOIN/02.sql
@@ -1,1 +1,1 @@
-SELECT testimonials.id AS id, writer.name AS writer, recipient.name AS recipient, testimonials.message AS message FROM testimonials JOIN users writer ON testimonials."writerId" = writer.id JOIN users recipient ON testimonials."recipientId" = recipient.id ORDER BY testimonials.id;
+SELECT testimonials.id, writer.name AS writer, recipient.name AS recipient, testimonials.message FROM testimonials JOIN users writer ON testimonials."writerId" = writer.id JOIN users recipient ON testimonials."recipientId" = recipient.id ORDER BY testimonials.id;
