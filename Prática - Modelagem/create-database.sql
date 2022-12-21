483cf609fdae28a47da5a6c433babb7b2b0716fe
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-21 11:50:32 -0300

    feat: create customers

--- Prática - Modelagem/create-database.sql
+++ Prática - Modelagem/create-database.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE "pratica_modelagem";
+
+CREATE TABLE "customers" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "cpf" VARCHAR(11) UNIQUE NOT NULL,
+    "email" TEXT UNIQUE NOT NULL,
+    "password" TEXT NOT NULL
+);
