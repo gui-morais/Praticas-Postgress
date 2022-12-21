4a2a4c0f000ce1204255942af38a7201f0916be7
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-21 11:59:47 -0300

    feat: create transactions

--- Prática - Modelagem/create-database.sql
+++ Prática - Modelagem/create-database.sql
@@ -10,9 +10,19 @@ CREATE TABLE "customers" (
 
 CREATE TABLE "bankAccount" (
     "id" SERIAL PRIMARY KEY NOT NULL,
-    "customerId" INTEGER REFERENCES customers(id) NOT NULL,
+    "customerId" INTEGER REFERENCES "customers"("id") NOT NULL,
     "accountNumber" TEXT NOT NULL UNIQUE,
     "agency" TEXT NOT NULL,
     "openDate" DATE NOT NULL DEFAULT NOW(),
     "closeDate" DATE
+);
+
+CREATE TABLE "transactions" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "bankAccountId" INTEGER NOT NULL REFERENCES "bankAccount"("id"),
+    "amount" INTEGER NOT NULL,
+    "type" ENUM("deposit", "withdraw") NOT NULL,
+    "time" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "description" TEXT,
+    "cancelled" BOOLEAN NOT NULL
 );
