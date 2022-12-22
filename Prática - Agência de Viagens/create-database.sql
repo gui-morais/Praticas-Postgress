0809f5d0ec997eaaaf41b19b02af8aa77f11f4ff
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-21 23:22:46 -0300

    feat: create flights and model

--- Prática - Agência de Viagens/create-database.sql
+++ Prática - Agência de Viagens/create-database.sql
@@ -11,3 +11,13 @@ CREATE TABLE "airports" (
     "name" TEXT NOT NULL,
     "code" VARCHAR(3) NOT NULL UNIQUE
 );
+
+CREATE TABLE "flights" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "code" TEXT NOT NULL UNIQUE,
+    "company_id" INTEGER NOT NULL REFERENCES "companies"("id"),
+    "origin_id" INTEGER NOT NULL REFERENCES "airports"("id"),
+    "destiny_id" INTEGER NOT NULL REFERENCES "airports"("id"),
+    "time_depart" TIME WITH TIME ZONE NOT NULL,
+    "time_arrival" TIME WITH TIME ZONE,
+);
