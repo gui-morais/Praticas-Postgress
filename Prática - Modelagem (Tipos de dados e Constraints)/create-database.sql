61d7acc564371c61a271270fe740d730a73d0582
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-21 22:48:14 -0300

    feat: create cart

--- Prática - Modelagem (Tipos de dados e Constraints)/create-database.sql
+++ Prática - Modelagem (Tipos de dados e Constraints)/create-database.sql
@@ -6,3 +6,10 @@ CREATE TABLE "users" (
     "email" TEXT NOT NULL UNIQUE,
     "password" TEXT NOT NULL
 );
+
+CREATE TABLE "cart" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "userid" INTEGER NOT NULL REFERENCES "users"("id");
+    "status" ENUM("criada", "paga", "entregue", "cancelada") NOT NULL,
+    "date" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
