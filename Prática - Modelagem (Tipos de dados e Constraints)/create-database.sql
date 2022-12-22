cc39566d065706a7d60720fb4d7cb04e9d69550c
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-21 22:41:38 -0300

    feat: create users

--- Prática - Modelagem (Tipos de dados e Constraints)/create-database.sql
+++ Prática - Modelagem (Tipos de dados e Constraints)/create-database.sql
@@ -1,1 +1,8 @@
 CREATE DATABASE "pratica_modelagem2";
+
+CREATE TABLE "users" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL UNIQUE,
+    "password" TEXT NOT NULL
+);
