abb81c467f45fca3a4c13b38dec4e14785fcec6c
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-21 23:35:10 -0300

    feat: create students

--- Prática - Curso/create-database.sql
+++ Prática - Curso/create-database.sql
@@ -1,1 +1,8 @@
 CREATE DATABASE "pratica_modelagem3";
+
+CREATE TABLE "students" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "name" TEXT NOT NULL,
+    "cpf" VARCHAR(11) NOT NULL UNIQUE,
+    "email" TEXT NOT NULL UNIQUE
+);
