53cd9db09fcf783da5aefaaccefad099dcb0c8aa
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-23 19:19:33 -0300

    feat: create table actors

--- Prática - Locadora/create-database.sql
+++ Prática - Locadora/create-database.sql
@@ -4,3 +4,10 @@ CREATE TABLE "countries" (
     "id" SERIAL PRIMARY KEY NOT NULL,
     "name" TEXT NOT NULL UNIQUE
 );
+
+CREATE TABLE "actors" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "name" TEXT NOT NULL,
+    "birthday" DATE NOT NULL,
+    "country_id" INTEGER NOT NULL REFERENCES "countries0"("id")
+);
